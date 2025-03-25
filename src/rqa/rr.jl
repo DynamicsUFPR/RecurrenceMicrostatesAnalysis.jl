8d2260e02b1c8877cffdc4d0fd34d81c0fa657d4
gabriel-ferr <gabriel.vferreira@icloud.com>
2025-03-25 09:51:54 -0300

    Add the setting 'num_samples' to function rrate

--- src/rqa/rr.jl
+++ src/rqa/rr.jl
@@ -26,10 +26,10 @@ end
 Compute the approximated recurrence rate of a Recurrence Plot from a dataset [x] using a probability distribution of recurrence microstates computed
 from it.
 """
-function rrate(x::AbstractArray, parameters, n::Int; shape::Symbol = :square, sampling_mode::Symbol = :random)
+function rrate(x::AbstractArray, parameters, n::Int; shape::Symbol = :square, sampling_mode::Symbol = :random, num_samples::Union{Float64, Int} = 0.05)
     ##
     ##      Compute the probabilities.
-    probs = distribution(x, parameters, n; shape = shape, sampling_mode = sampling_mode)
+    probs = distribution(x, parameters, n; shape = shape, sampling_mode = sampling_mode, num_samples = num_samples)
 
     return rrate(probs)
 end
