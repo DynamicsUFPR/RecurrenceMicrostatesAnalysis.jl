56219cc6b76d58e8764e134bcb6648ba5e45e61e
gabriel-ferr <gabriel.vferreira@icloud.com>
2025-03-26 23:18:17 -0300

    Fix an issue with memory. TODO: increase the performance

--- src/rma/recurrence.jl
+++ src/rma/recurrence.jl
@@ -31,14 +31,14 @@ just used for high-dimensional problems, such as images.
 These functions return `true` when we have a recurrence, and `false` otherwise.
 """
 @inline function recurrence(x::Matrix{Float64}, y::Matrix{Float64}, threshold::Float64, idx::AbstractVector{Int}, metric::Metric, _::AbstractVector{Int})
-    return @inbounds evaluate(metric, x[:, idx[1]], y[:, idx[2]]) <= threshold
+    return @inbounds evaluate(metric, view(x, :, idx[1]), view(y, :, idx[2])) <= threshold
 end
 
 @inline function recurrence(x::Matrix{Float64}, y::Matrix{Float64}, thresholds::Tuple{Float64, Float64}, idx::AbstractVector{Int}, metric::Metric, _::AbstractVector{Int})
-    distance = @inbounds evaluate(metric, x[idx[:, 1]], y[idx[:, 2]])
+    distance = @inbounds evaluate(metric, view(x, :, idx[1]), view(y, :, idx[2]))
     return (distance > thresholds[1] && distance <= thresholds[2])
 end
-
+    
 # ----------------------------------      RECURRENCE TENSOR SPACE      ----------------------------------------------
 @inline function recurrence(x::AbstractArray, y::AbstractArray, threshold::Float64, idx::AbstractVector{Int}, metric::Metric, dim::AbstractVector{Int})
     return @inbounds evaluate(metric, view(x, :, view(idx, 1:dim[1])), view(y, :, view(idx, dim[1]+1:dim[1] + dim[2]))) <= threshold
