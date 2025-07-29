3ae123c0169d349a5a3850dac17d59c230c3c433
gabriel-ferr <gabriel.vferreira@icloud.com>
2025-07-29 14:57:59 -0300

    fix jrp function

--- src/rma/recurrence.jl
+++ src/rma/recurrence.jl
@@ -56,8 +56,8 @@ end
 end
 
 # ----------------------------------      JRP      ----------------------------------------------
-@inline function jrp(x::Matrix{Float64}, y::Matrix{Float64}, threshold::Float64, idx::AbstractVector{Int}, metric, _::AbstractVector{Int})
-    return @inbounds (evaluate(metric, view(x, :, idx[1]), view(x, :, idx[2])) <= threshold) && (evaluate(metric, view(y, :, idx[1]), view(y, :, idx[2])) <= threshold)
+@inline function jrp(x::Matrix{Float64}, y::Matrix{Float64}, thresholds::Float64, idx::AbstractVector{Int}, metric, _::AbstractVector{Int})
+    return @inbounds (evaluate(metric, view(x, :, idx[1]), view(x, :, idx[2])) <= thresholds[1]) && (evaluate(metric, view(y, :, idx[1]), view(y, :, idx[2])) <= thresholds[2])
 end
 
 
