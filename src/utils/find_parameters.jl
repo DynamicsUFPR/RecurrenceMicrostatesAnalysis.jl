718012915eeae185cd77e83ece877ebeecc6df31
FelipePiaceski <89945263+FelipePiaceski@users.noreply.github.com>
2025-11-19 19:11:40 -0300

    Add v2_find_parameters function with disorder input
    The new version of the function allows for a disorder function to be passed as an argument.

--- src/utils/find_parameters.jl
+++ src/utils/find_parameters.jl
@@ -56,3 +56,52 @@ function find_parameters(
 
     return εopt, Smax
 end
+
+
+# New version, capable of receiving the disorder function as an input as well
+function v2_find_parameters(
+        x::AbstractArray,
+        n::Int;
+        r::Float64 = 0.05,
+        ε_max_range::Float64 = 0.5,
+        fraction::Int = 5,
+        shape::Symbol = :square,
+        F::Function = rentropy 
+    )
+
+    !(F in [rentropy, disorder]) && throw("Function must be rentropy or disorder.")
+    kwargs = (F == rentropy) ? (;) : (; shape = shape)
+    
+    ε::Float64 = 1e-6
+    εopt::Float64 = 0.0
+    if length(x) <= 1000
+        εopt = maximum(pairwise(Euclidean(), eachrow(x))) * (ε_max_range - ε)
+    else
+        εopt = (maximum(x) - minimum(x)) * size(x, 2)
+    end
+    Δε::Float64 = (εopt - ε) / fraction
+
+    fmax::Float64 = 0.0
+
+    ##      Just to allocate memory.
+    dist::Vector{Float64} = distribution(x, 0.01, n; num_samples = 2, shape = shape)
+
+    for _ ∈ 1:fraction
+        for _ ∈ 1:fraction
+            dist = distribution(x, ε, n; num_samples = r, shape = shape)
+            f = F(dist; kwargs...)
+
+            if f > fmax
+                fmax = f
+                εopt = ε
+            end
+
+            ε += Δε
+        end
+
+        ε = εopt - Δε
+        Δε *= 2 / fraction
+    end
+
+    return εopt, fmax
+end
